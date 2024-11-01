8775b2f74aab1f5f75df9514988e807e8ced56ac
Carl D Kennedy <116731613+Carldkennedy@users.noreply.github.com>
2024-11-01 16:54:35 +0000

    Call functions with dot notation

--- parser/lmod.py
+++ parser/lmod.py
@@ -1,14 +1,14 @@
 import subprocess
 import os
 import config
-from utils import append_file, append_log
+import utils
 
 def run_collect_data_script():
     try:
         result = subprocess.run(['python', 'collect_data.py'], check=True) 
         return result.returncode == 0
     except subprocess.CalledProcessError as e:
-        append_log(f"Failed to run collect_data.py: {e}", config.main_log_file)
+        utils.append_log(f"Failed to run collect_data.py: {e}", config.main_log_file)
         print(f"Failed to run collect_data.py: {e}")
         return False
 
@@ -31,20 +31,20 @@ def process_broken_symlinks():
     with open(config.broken_symlinks_file, 'r') as file:
         broken_symlinks = file.readlines()
 
-    append_file(config.broken_symlinks_file, f"\n\nls -lrtah <file not found>\n")
-    append_file(config.broken_symlinks_file, f"\nls -lrath <symlink target>\n\n")
+    utils.append_file(config.broken_symlinks_file, f"\n\nls -lrtah <file not found>\n")
+    utils.append_file(config.broken_symlinks_file, f"\nls -lrath <symlink target>\n\n")
     for symlink in broken_symlinks:
         symlink = symlink.strip()
         if symlink:
             result = subprocess.run(['ls', '-lrtah', symlink], capture_output=True, text=True)
-            append_file(config.broken_symlinks_file, result.stdout)
-            append_file(config.broken_symlinks_file, result.stderr)
+            utils.append_file(config.broken_symlinks_file, result.stdout)
+            utils.append_file(config.broken_symlinks_file, result.stderr)
 
             target = os.path.realpath(symlink)
             if target:
                 result = subprocess.run(['ls', '-lrath', target], capture_output=True, text=True)
-                append_file(config.broken_symlinks_file, result.stdout)
-                append_file(config.broken_symlinks_file, result.stderr)
+                utils.append_file(config.broken_symlinks_file, result.stdout)
+                utils.append_file(config.broken_symlinks_file, result.stderr)
 
 def extract_package_info(collected_data):
     """
